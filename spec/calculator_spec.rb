83b9e7320bd811b193f8ba88444f90c4358ca5c9
Joel Howard <joelhoward2@gmail.com>
2023-03-31 18:42:48 -0400

    Complete code to finish exercise

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -27,21 +27,68 @@ describe Calculator do
   describe '#subtract' do
     # Write a test for the subtract method
     # Hint: Follow the Arrange, Act, Assert pattern
+    it 'returns the difference between two numbers' do
+      # Arrange:
+      calculator = Calculator.new
+      num1 = 2
+      num2 = 3
+      # Act:
+      result = calculator.subtract(num1, num2)
+
+      # Assert:
+      expect(result).to eq(-1)
+    end
   end
 
   describe '#multiply' do
     # Write a test for the multiply method
     # Hint: Follow the Arrange, Act, Assert pattern
+    it 'returns the product of two numbers' do
+      # Arrange:
+      calculator = Calculator.new
+      num1 = 5
+      num2 = 3
+
+      # Act:
+      result = calculator.multiply(num1, num2)
+
+      # Assert:
+      expect(result).to eq(15)
+    end
   end
 
   describe '#divide' do
     # Write a test for the divide method
     # Hint: Follow the Arrange, Act, Assert pattern
+
+    it 'returns the quotient of two numbers' do
+      # Arrange:
+      calculator = Calculator.new
+      num1 = 350
+      num2 = 2
+
+      # Act:
+      result = calculator.divide(num1, num2)
+
+      # Assert:
+      expect(result).to eq(175)
+    end
   end
 
   describe '#square' do
     # Write a test for the square method
     # Hint: Follow the Arrange, Act, Assert pattern
+    it 'returns the square of a number' do
+      # Arrange:
+      calculator = Calculator.new
+      num = 5
+
+      # Act:
+      result = calculator.square(num)
+
+      # Assert:
+      expect(result).to eq(25)
+    end
   end
 
   # Feel free to add more tests for additional methods in the Calculator class
